50cc6b7385a47c503e8e695e441911c74925b728
Emanuele Petriglia <inbox@emanuelepetriglia.me>
2017-04-01 16:39:59 +0200

    Add a basic installer script and some dotfiles
    The script is temporary, because the files to be linked are currently
    hard coded.

---
+++ apply.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Crea i collegamenti simbolici.
+
+# -s : solo link simbolici;
+# -f : rimuovere il file di destinazione se esistente;
+# -v : verboso.
+LN="ln -svf"
+
+# TODO: evitare di inserire i file come valori cablati.
+$LN ~/.dotfiles/vim/vimrc ~/.vimrc
+$LN ~/.dotfiles/vim/colors/solarized.vim ~/.vim/colors/solarized.vim
+
+$LN ~/.dotfiles/bash/bash_aliases ~/.bash_aliases
+
+$LN ~/.dotfiles/git/gitconfig ~/.gitconfig
+
+$LN ~/.dotfiles/terminator/config ~/.config/terminator/config
+
+$LN ~/.dotfiles/gnupg/gpg.conf ~/.gnupg/gpg.conf
+$LN ~/.dotfiles/gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
