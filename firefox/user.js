7b5d2e63e1eee6613ab8f4653f65d40925db3a4a
Emanuele Petriglia <inbox@emanuelepetriglia.me>
2018-06-24 16:01:12 +0200

    Update user.js for Mozilla Firefox

--- firefox/user.js
+++ firefox/user.js
@@ -1,93 +1,184 @@
-
 /**
- * My custom preferences that are appended after ghacks-user.js.
+ * Custom preferences for Mozilla Firefox.
  *
- * Every option that is overrided has a number that explain the purpose of that
- * option.
+ * WARNING: this file must be appended before ghacks-userjs, because these are
+ * only personal settings, but I use also default settings.
  */
 
 user_pref("_user.js.parrot", "syntax error @ BEGIN USER CUSTOMIZATIONS");
 
-user_pref("browser.startup.page", 1); // 0102
-user_pref("browser.startup.homepage", "https://start.duckduckgo.com/"); // 0103
+/**
+ * 0100: STARTUP
+ */
 
-user_pref("browser.search.countryCode", "IT"); // 0202
-user_pref("browser.search.region", "IT"); // 0202
+/** 0102: set start page to a custom URL. **/
+user_pref("browser.startup.page", 1);
 
-user_pref("intl.locale.matchOS", true); // 0203
+/** 0103: custom URL for start page. **/
+user_pref("browser.startup.homepage", "https://start.duckduckgo.com/");
 
-user_pref("intl.accept_languages", "it-IT, it, en-US, en"); // 0207
 
-user_pref("javascript.use_us_english_locale", false); // 0208
+/**
+ * 0200: GEOLOCALIZATION
+ */
 
-user_pref("browser.safebrowsing.malware.enabled", false); // 0410
-user_pref("browser.safebrowsing.phishing.enabled", false); // 0410
+/** 0201: disable geolocalization. **/
+user_pref("geo.enabled", false);
 
-user_pref("privacy.trackingprotection.enabled", true); // 0420
+/** 0202: disable geolocalization for search results. **/
+user_pref("browser.search.countryCode", "UK");
+user_pref("browser.search.region", "UK");
 
-user_pref("urlclassifier.trackingTable", "test-track-simple,base-track-digest256,content-track-digest256"); // 0422
+/** 0205: set OS & APP locale. **/
+user_pref("intl.locale.requested", "en-UK");
 
-user_pref("places.history.enabled", false); // 0862
+/** 0207: set language to match. **/
+user_pref("intl.accept_languages", "it-IT, it, en-UK, en");
 
-user_pref("browser.urlbar.autocomplete.enabled", true); // 0850a
-user_pref("browser.urlbar.suggest.bookmark", true); // 0850a
-user_pref("browser.urlbar.suggest.history", true); // 0850a
 
-user_pref("browser.urlbar.autoFill", true); // 0850d
-user_pref("browser.urlbar.autoFill.typed", true); // 0850d
+/**
+ * 0300: AUTO UPDATING
+ */
 
-user_pref("signon.rememberSignons", false); // 0901
+/** 0301a: enable auto-update checks for Firefox. **/
+user_pref("app.update.enabled", true);
 
-user_pref("browser.sessionstore.max_tabs_undo", 10); // 1020
-user_pref("browser.sessionstore.max_windows_undo", 3); // 1020
+/* 0301b: enable auto-update checks for extensions. **/
+user_pref("extensions.update.enabled", true);
 
-user_pref("full-screen-api.enabled", true); // 2418
+/* 0302b: enable auto update installing for extensions. **/
+user_pref("extensions.update.autoUpdateDefault", true);
 
-user_pref("layers.acceleration.disabled", false); // 2508
 
-user_pref("browser.download.folderList", 1); // 2602
+/**
+ * 0800: LOCATION BAR / SEARCH BAR / SUGGESTIONS / HISTORY / FORMS [SETUP]
+ */
 
-user_pref("browser.download.useDownloadDir", true); // 2603
+/** 0850a: enable location bar autocomplete and suggestion types. **/
+user_pref("browser.urlbar.autocomplete.enabled", true);
+user_pref("browser.urlbar.suggest.bookmark", true);
+user_pref("browser.urlbar.suggest.history", true);
+user_pref("browser.urlbar.suggest.openpage", true);
 
-user_pref("browser.download.forbid_open_with", false); // 2662
+/** 0850d: enable location bar autofill. **/
+user_pref("browser.urlbar.autoFill", true);
+user_pref("browser.urlbar.autoFill.typed", true);
 
-user_pref("network.cookie.cookieBehavior", 1); // 2701
 
-user_pref("network.cookie.thirdparty.sessionOnly", false); // 2702
+/**
+ * 0900: PASSWORDS
+ */
 
-user_pref("privacy.clearOnShutdown.cache", false); // 2803
-user_pref("privacy.clearOnShutdown.sessions", false); //  2803
+/** 0901: disable saving passwords. **/
+user_pref("signon.rememberSignons", false);
 
-user_pref("clipboard.autocopy", false); // 5005
 
-user_pref("alerts.showFavicons", true); // 1031
+/**
+ * 1000: CACHE [SETUP]
+ */
 
-user_pref("browser.newtabpage.directory.ping", "data:text/plain,"); // 0360
+/** 1020: enable the Session Restore service. **/
+user_pref("browser.sessionstore.max_tabs_undo", 10);
+user_pref("browser.sessionstore.max_windows_undo", 3);
 
-user_pref("security.ssl3.dhe_rsa_aes_128_sha", false); // 1263
-user_pref("security.ssl3.dhe_rsa_aes_256_sha", false); // 1263
 
-user_pref("clipboard.autocopy", false); // 5005
+/**
+ * 1400: FONTS
+ */
 
-user_pref("privacy.userContext.enabled", true); // 1702
+/** 1401: enable websites choosing fonts.
+ * Note: I use uBlock Origin to block by default all fonts.
+ */
+user_pref("browser.display.use_document_fonts", 1);
 
-user_pref("keyword.enabled", true); // 0801
 
-// Prefs not documented.
+/**
+ * 1700: CONTAINERS [SETUP]
+ */
 
-// Set the default search engine to DuckDuckGo.
+/** 1701: enable Container Tabs setting in preferences. **/
+user_pref("privacy.userContext.ui.enabled", true);
+
+/** 1702: enable Container Tabs. **/
+user_pref("privacy.userContext.enabled", true);
+
+
+/**
+ * 2500: HARDWARE FINGERPRINTING
+ */
+
+/** 2508: enable hardware acceleration (for performances). **/
+user_pref("layers.acceleration.disabled", false);
+
+
+/**
+ * 2600: MISCELLANEOUS
+ */
+
+/** 2650: default downloads folder. **/
+user_pref("browser.download.folderList", 1);
+
+/** 2651: do not ask everytime where to download a file. **/
+user_pref("browser.download.useDownloadDir", true);
+
+/** 2652: adds downloads to the system's "recent documents" list.
+ * Note: thist list is cleared on exiting Firefox!
+ */
+user_pref("browser.download.manager.addToRecentDocs", true);
+
+/** 2654: enable "open with" in download dialog. **/
+user_pref("browser.download.forbid_open_with", false);
+
+
+/**
+ * 2700: PERSISTENT STORAGE
+ */
+
+/** 2701: enable cookies only for the same host. **/
+user_pref("network.cookie.cookieBehavior", 1);
+
+
+/**
+ * 2800: SHUTDOWN [SETUP]
+ */
+
+/** 2803: set what history items to clear on shutdown. **/
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown.offlineApps", false);
+
+
+/**
+ * 5000: PERSONAL [SETUP]
+ */
+
+/** WARNINGS **/
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.warnOnOpen", false);
+
+/** Disable autocopy default. **/
+user_pref("clipboard.autocopy", false);
+
+/**
+ * Not documented by ghacks-userjs
+ */
+
+/** Set DuckDuckGo as default search engine. **/
 user_pref("browser.search.defaultenginename", "DuckDuckGo");
 user_pref("browser.search.order.1", "DuckDuckGo");
 
+/** Disable syncronization of some personal informations. **/
 user_pref("services.sync.engine.history", false);
 user_pref("services.sync.engine.passwords", false);
 user_pref("services.sync.engine.prefs", false);
 user_pref("services.sync.engine.tabs", false);
 
-// UI-element-popup-tips.
+/** Disable UI-element-popup-tips. **/
 user_pref("browser.chrome.toolbar_tips", false);
 
-// Mouse scroll speed.
+/** Increase mouse scroll speed. **/
 user_pref("mousewheel.default.delta_multiplier_y", 200);
+
 
 user_pref("_user.js.parrot", "user settings loaded");
