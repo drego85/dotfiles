b3c32dc17695d8c3bb8b1ccfcec3ad775b65c121
Emanuele Petriglia <inbox@emanuelepetriglia.me>
2018-10-20 12:56:59 +0200

    Improve doc and function's parameters

--- .common/utils.sh
+++ .common/utils.sh
@@ -5,46 +5,50 @@
 # Copyright (c) 2018 Emanuele Petriglia <inbox@emanuelepetriglia.me>
 # All right reserved. This file is licensed under the MIT license.
 
+# List of functions can be used (note: all functions start with 'ed'):
+#   - ed_log: it prints a message to the standard output;
+#   - ed_wlog: it prints a message to the standard error;
+#   - ed_elog: it prints a message to the standard error and exits;
+#
+
 # Logging {{{
 
-# Prints a message to the standard output.
+# It prints a message to the standard output.
 #
 # Parameter:
-#     $1 -> the message to print to the standard output;
-#     $2 -> if "true" enables the message "$1" to be printed.
+#     $1 -> the message to print to the standard output.
 #
 # Example:
-#     ed_log "Some application downloaded!" true
+#     ed_log "Some application downloaded!"
+#
+# Note:
+#     This function exists only to have a common name with other functions,
+#     otherwise 'echo' has the same behaviour.
 ed_log() {
-  if [[ "$2" == "true" ]]; then
-    echo "$1"
-  fi
+  echo "$1"
 }
 
 # Prints a message to the standard error.
 #
 # Parameter:
-#     $1 -> the message to print to the standard error;
-#     $2 -> if "true" enables the message "$1" to be printed.
+#     $1 -> the message to print to the standard error.
 #
 # Example:
-#     ed_wlog "Skipping some tasks!" true
+#     ed_wlog "Skipping some tasks!"
 ed_wlog() {
-  if [[ "$2" == "true" ]]; then
-    echo "$1" >&2
-  fi
+  echo "$1" >&2
 }
 
-# Prints a message to the standard error and exit with an error code.
+# It prints a message to the standard error and exits with an error code.
 #
-# Parameter:
+# Parameters:
 #     $1 -> the message to print to the standard error;
 #     $2 -> the error code, default is 1.
 #
 # Example:
 #     ed_elog "X not found!" 5
 ed_elog() {
-  local code=${2:-1}
+  local code=${2:-1} # If $2 is unset or empty, set 'code' to 1.
 
   echo "$1" >&2
   exit $code
@@ -62,6 +66,7 @@ ed_elog() {
 # Example:
 #     ed_make_symlink path/to/source path/to/dst
 ed_make_symlink() {
+  echo "WIP"
 }
 
 # vim:foldmethod=marker
